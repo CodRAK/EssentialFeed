5b3b22c2eb53a270a9aac945eef4684b9728e7cd
codRAK <aseemkapoor88@gmail.com>
2022-08-06 17:18:13 +0530

    Initial Commit with some files for Essential Feed

---
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by aseem kapoor on 06/08/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
