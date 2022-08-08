85ecf165c461e91b12878d1cbe5c794110d5d0db
codRAK <aseemkapoor88@gmail.com>
2022-08-08 19:50:17 +0530

    Delivers connectivity Error on Client Error

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -8,19 +8,25 @@
 import Foundation
 
 public protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Error) -> Void)
 }
 
 public final class RemoteFeedLoader {
     private let url: URL
     private let httpClient: HTTPClient
     
+    public enum Error: Swift.Error {
+        case connectivity
+    }
+    
     public init(url: URL, client: HTTPClient) {
         self.httpClient = client
         self.url = url
     }
     
-    public func load() {
-        httpClient.get(from: url)
+    public func load(completion: @escaping (Error) -> Void = {_ in }) {
+        httpClient.get(from: url) { error in
+            completion(.connectivity)
+        }
     }
 }
